da96ed28c8e69b183c6a9b3fc09022fddda78b5a
Logan Bolton <loganbolton101@gmail.com>
2024-03-21 14:32:09 -0500

    nice connect4 game

--- game.py
+++ game.py
@@ -1,1 +1,89 @@
-print("hello world")
+import numpy as np
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+    return np.zeros((ROW_COUNT, COLUMN_COUNT))
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    if col < 0 or col >= COLUMN_COUNT:
+        return False
+    return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Check positively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # Check negatively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        col = int(input("Player 1 Make Your Selection (0-6):"))
+        while is_valid_location(board, col) == False:
+            print(f"{col} is not a valid column")
+            col = int(input("Player 1 Make Your Selection (0-6):"))
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, 1)
+
+        if winning_move(board, 1):
+            print_board(board)
+            print("PLAYER 1 WINS!")
+            game_over = True
+            exit()
+
+    # Ask for Player 2 Input
+    else:
+        col = int(input("Player 2 Make Your Selection (0-6):"))
+        while is_valid_location(board, col) == False:
+            print(f"{col} is not a valid column")
+            col = int(input("Player 2 Make Your Selection (0-6):"))
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, 2)
+
+        if winning_move(board, 2):
+            print_board(board)
+            game_over = True
+            print("PLAYER 2 WINS!")
+            exit()
+
+    print_board(board)
+
+    turn += 1
+    turn = turn % 2
+
